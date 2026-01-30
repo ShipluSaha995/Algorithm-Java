7468ba87c1c040acd6593daf3a1dae61720542ca
ShipluSaha995 <shiplusaha995@gmail.com>
2026-01-30 08:45:02 +0600

    Graph

---
+++ Graoh.java
@@ -0,0 +1,18 @@
+/*
+Graph:
+
+A graph is a non-linear data-structure used to represent relationship between objects 
+
+A graph is defiend as G(V,E) Where,
+        V = a finite set of vertices(nodes)
+        E = a set of edges connecting pairs of vertices
+
+we can say that, A Graph consits of a non-empty set of vertices or nodes V and a set of edges E.
+
+
+
+
+
+
+
+*/

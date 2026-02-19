0609b0db6f11c6be192865fe703bf73a922c65b4
ShipluSaha995 <shiplusaha995@gmail.com>
2026-02-19 21:02:42 +0600

    Update readme

---
+++ Topological_sort.cpp
@@ -0,0 +1,16 @@
+//Topological sort:
+
+/*
+Topological sort of a directed graph is a linear ordering of its vertices such that 
+every directed edges uv come from vertices u to v, where u comes before v 
+
+1. Graph has to be directed
+2. There should be no cycle in the graph
+
+Method:
+
+1. Find the indegree(number of incoming edges) of all vertices.
+2. Print all the vertices whose indegree is 0 . 
+
+*/
+

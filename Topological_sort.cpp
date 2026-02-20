5033bb32f16c4023414152121f4166f94a1331db
ShipluSaha995 <shiplusaha995@gmail.com>
2026-02-20 11:36:18 +0600

    Update readme

--- Topological_sort.cpp
+++ Topological_sort.cpp
@@ -14,3 +14,56 @@ Method:
 
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+void topology(int V, vector<vector<int>> &adj){
+    vector<int>indegree(V, 0);
+
+    for(int i=0; i<V; i++){
+        for(int V:adj[i]){
+            indegree[V]++;
+        }
+    }
+
+    queue<int>q;
+    for(int i=0; i<V; i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+
+    int count=0;
+
+    while(!q.empty()){
+        int u=q.front();
+        q.pop();
+        cout<<u<<" ";
+        count++;
+        
+        for(int v:adj[u]){
+            indegree[v]--;
+            if(indegree[v]==0){
+                q.push(v);
+            }
+        }
+    }
+
+    if(count!=V){
+        cout<<"have a cycle"<<"\n";
+    }
+}
+
+int main(){
+
+    int V, E;
+    cin>>V>>E;
+    vector<vector<int>>adj(V);
+
+    for(int i=0; i<E; i++){
+        int u, v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+    }
+
+    topology(V, adj);
+}
